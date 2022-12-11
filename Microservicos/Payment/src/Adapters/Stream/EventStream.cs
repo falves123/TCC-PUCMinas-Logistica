3d8350430b056569b5d668e05747b2b004a4aac1
Raphael Sato <raphael.sato@gmail.com>
2022-12-10 23:37:33 -0500

    Integracao entre Order e Payment via RabbitMQ

--- Microservicos/Payment/src/Adapters/Stream/EventStream.cs
+++ Microservicos/Payment/src/Adapters/Stream/EventStream.cs
@@ -3,6 +3,16 @@ public class EventStream : EventStreamBase, IEventStream
 {
     public override void StreamEvents()
     {
+        Subscribe<IPaymentService, OrderCreatedEventDTO>("Stream1", "OrderCreated", (dto, paymentService, Dp) =>
+        {
+            var command = new Payment()
+            {
+                CustomerName = dto.CustomerName,
+                OrderID = dto.ID,
+                Value = dto.Total
+            };
 
+            paymentService.Add(command);
+        });
     }
 }
